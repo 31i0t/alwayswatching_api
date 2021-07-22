727dafeb6d8ce1a5f6f36d1544a35ab3388ab273
Eliot Makoski <eliot@makoski.net>
2021-07-23 06:13:27 +1000

    Docker Compose setup

--- alwayswatching_django/asgi.py
+++ alwayswatching_django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for alwayswatching project.
+ASGI config for alwayswatching_django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alwayswatching.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alwayswatching_django.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alwayswatching.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alwayswatching_django.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
